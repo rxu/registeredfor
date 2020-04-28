3a3414a215a2154cdf1a772e633f86dc797ad715
rxu <rxu@mail.ru>
2020-04-28 20:00:05 +0700

    Fix guest posts error. Also don't show membership timespan info for guests.

--- event/main_listener.php
+++ event/main_listener.php
@@ -55,43 +55,46 @@ class main_listener implements EventSubscriberInterface
 	 */
 	public function add_registered_for_info($event, $eventname)
 	{
-		switch ($eventname)
+		if ((int) $event['poster_id'] != ANONYMOUS)
 		{
-			case 'core.viewtopic_cache_user_data':
-				$user_cache_data = $event['user_cache_data'];
-				$user_cache_data['user_regdate'] = $event['row']['user_regdate'];
+			switch ($eventname)
+			{
+				case 'core.viewtopic_cache_user_data':
+					$user_cache_data = $event['user_cache_data'];
+					$user_cache_data['user_regdate'] = $event['row']['user_regdate'];
 
-				$event['user_cache_data'] = $user_cache_data;
-			break;
+					$event['user_cache_data'] = $user_cache_data;
+				break;
 
-			case 'core.viewtopic_modify_post_row':
-				$this->language->add_lang('registeredfor', 'rxu/registeredfor');
+				case 'core.viewtopic_modify_post_row':
+					$this->language->add_lang('registeredfor', 'rxu\registeredfor');
 
-				$post_row = $event['post_row'];
-				$user_poster_data = $event['post_row'];
+					$post_row = $event['post_row'];
+					$user_poster_data = $event['post_row'];
 
-				$current_time = time();
-				$seconds_delta = $current_time - (int) $event['user_poster_data']['user_regdate'];
+					$current_time = time();
+					$seconds_delta = $current_time - (int) $event['user_poster_data']['user_regdate'];
 
-				$datetime_new = date_create('@' . (string) $current_time);
-				$datetime_old = date_create('@' . (string) $event['user_poster_data']['user_regdate']);
-				$interval = date_diff($datetime_new, $datetime_old);
+					$datetime_new = date_create('@' . (string) $current_time);
+					$datetime_old = date_create('@' . (string) $event['user_poster_data']['user_regdate']);
+					$interval = date_diff($datetime_new, $datetime_old);
 
-				$days_in_current_month = (int) date('t');
-				$days_in_current_year = 365 + (int) date('L');
+					$days_in_current_month = (int) date('t');
+					$days_in_current_year = 365 + (int) date('L');
 
-				$time = [];
-				($interval->y && $seconds_delta > 86400 * $days_in_current_year) ? 		$time[] = $this->language->lang('D_YEAR', $interval->y) : null;
-				($interval->m && $seconds_delta > 86400 * $days_in_current_month) ? 	$time[] = $this->language->lang('D_MON', $interval->m) : null;
-				($interval->d && $seconds_delta > 86400) ? 								$time[] = $this->language->lang('D_MDAY', $interval->d) : null;
-				($interval->h && $seconds_delta >= 3600  && $seconds_delta < 86400) ?	$time[] = $this->language->lang('D_HOURS', $interval->h) : null;
-				($interval->i && $seconds_delta >= 60 && $seconds_delta < 86400) ? 		$time[] = $this->language->lang('D_MINUTES', $interval->i) : null;
-				($interval->s && $seconds_delta < 60) ? 								$time[] = $this->language->lang('D_SECONDS', $interval->s) : null;
+					$time = [];
+					($interval->y && $seconds_delta > 86400 * $days_in_current_year) ? 		$time[] = $this->language->lang('D_YEAR', $interval->y) : null;
+					($interval->m && $seconds_delta > 86400 * $days_in_current_month) ? 	$time[] = $this->language->lang('D_MON', $interval->m) : null;
+					($interval->d && $seconds_delta > 86400) ? 								$time[] = $this->language->lang('D_MDAY', $interval->d) : null;
+					($interval->h && $seconds_delta >= 3600  && $seconds_delta < 86400) ?	$time[] = $this->language->lang('D_HOURS', $interval->h) : null;
+					($interval->i && $seconds_delta >= 60 && $seconds_delta < 86400) ? 		$time[] = $this->language->lang('D_MINUTES', $interval->i) : null;
+					($interval->s && $seconds_delta < 60) ? 								$time[] = $this->language->lang('D_SECONDS', $interval->s) : null;
 
-				$post_row['POSTER_JOINED'] = implode(' ', $time);
+					$post_row['POSTER_JOINED'] = implode(' ', $time);
 
-				$event['post_row'] = $post_row;
-			break;
+					$event['post_row'] = $post_row;
+				break;
+			}
 		}
 	}
 
@@ -102,6 +105,9 @@ class main_listener implements EventSubscriberInterface
 	 */
 	public function replace_joined_lang_entry($event)
 	{
-		$this->template->assign_var('L_JOINED', $this->language->lang('REGISTEREDFOR'));
+		if ((int) $event['topic_data']['topic_poster'] != ANONYMOUS)
+		{
+			$this->template->assign_var('L_JOINED', $this->language->lang('REGISTEREDFOR'));
+		}
 	}
 }
